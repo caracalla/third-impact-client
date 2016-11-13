a97dfcb25432528845ec0c15a0687fc660ac3164
James Anderson <caracallarex@gmail.com>
2016-11-12 16:34:32 -0800

    Authentication and sign up

--- vanilla-js/script.js
+++ vanilla-js/script.js
@@ -1,54 +1,51 @@
 (function (window, document, undefined) {
-  var base_url = "http://localhost:3000/"
-  var output = document.getElementById("output");
+  var baseURL = "http://localhost:3000/"
+  var mainElement = document.getElementById("main-element");
   var brandLink = document.getElementById("brand-link");
   var homeLink = document.getElementById("home-link");
-
-  var processPosts = function (posts) {
-    var output = [];
-
-    for (var i = 0; i < posts.length; i++) {
-      output[i] = templates.post(posts[i]);
-    }
-
-    return output.join("\n");
-  };
+  var usersLink = document.getElementById("users-link");
+  var logInLink = document.getElementById("log-in-link");
+  var signUpLink = document.getElementById("sign-up-link");
 
   var makeUserLinkHandlers = function () {
     var userLinks = document.getElementsByClassName("user-link");
 
     for (var i = 0; i < userLinks.length; i++) {
-      userLinks[i].addEventListener("click", function (userLink) {
+      userLinks[i].onclick = function (userLink) {
         return function (event) {
           getUser(userLink.dataset.userid);
         };
-      }(userLinks[i]));
+      }(userLinks[i]);
     }
   };
 
   var getRequest = function (url, element, func) {
-    element.innerHTML =
-      '<div class="sk-circle">'
-    +   '<div class="sk-circle1 sk-child"></div>'
-    +   '<div class="sk-circle2 sk-child"></div>'
-    +   '<div class="sk-circle3 sk-child"></div>'
-    +   '<div class="sk-circle4 sk-child"></div>'
-    +   '<div class="sk-circle5 sk-child"></div>'
-    +   '<div class="sk-circle6 sk-child"></div>'
-    +   '<div class="sk-circle7 sk-child"></div>'
-    +   '<div class="sk-circle8 sk-child"></div>'
-    +   '<div class="sk-circle9 sk-child"></div>'
-    +   '<div class="sk-circle10 sk-child"></div>'
-    +   '<div class="sk-circle11 sk-child"></div>'
-    +   '<div class="sk-circle12 sk-child"></div>'
-    + '</div>'
+    element.innerHTML = templates.spinner();
 
     var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
     xhr.open("get", url);
+
+    if (isLoggedIn()) {
+      xhr.setRequestHeader("X-Auth-Email", localStorage["auth-email"]);
+      xhr.setRequestHeader("X-Auth-Token", localStorage["auth-token"]);
+    }
 
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         func(element, JSON.parse(xhr.responseText));
+
+        if (isLoggedIn()) {
+          showLoggedInState();
+        }
+      } else if (xhr.readyState === 4 && xhr.status === 401) {
+        localStorage.removeItem("auth-email");
+        localStorage.removeItem("auth-token");
+        localStorage.removeItem("username");
+        localStorage.removeItem("user-id");
+
+        showLoggedOutState();
+        getPosts();
       } else {
         element.innerHTML = "Error: " + xhr.status.toString();
       }
@@ -57,34 +54,194 @@
     xhr.send(null);
   }
 
-  var getPosts = function () {
-    var posts_url = base_url + "posts";
+  // Posts
 
-    getRequest(posts_url, output, function (element, posts) {
-      element.innerHTML = processPosts(posts);
+  var getPosts = function () {
+    var postsURL = baseURL + "posts";
+
+    getRequest(postsURL, mainElement, function (element, posts) {
+      if (isLoggedIn()) {
+        element.innerHTML = templates.postForm() + posts.map(templates.post).join("\n");
+        var submitPostButton = document.getElementById("submit-post-button");
+        submitPostButton.onclick = postPost;
+      } else {
+        element.innerHTML = posts.map(templates.post).join("\n");
+      }
+
+      makeUserLinkHandlers();
+    });
+  };
+
+  var getUserPosts = function (userid) {
+    var userPostsURL = baseURL + "users/" + userid + "/posts";
+    var element = document.getElementById("user-posts")
+
+    getRequest(userPostsURL, element, function (element, posts) {
+      element.innerHTML = posts.map(templates.post).join("\n");
+      makeUserLinkHandlers();
+    });
+  };
+
+  // Users
+
+  var getUsers = function () {
+    var usersURL = baseURL + "users";
+
+    getRequest(usersURL, mainElement, function(element, users) {
+      element.innerHTML = templates.users(users)
       makeUserLinkHandlers();
     });
   };
 
   var getUser = function (userid) {
-    var user_url = base_url + "users/" + userid;
+    var userURL = baseURL + "users/" + userid;
 
-    getRequest(user_url, output, function (element, user) {
-      element.innerHTML = window.templates.user(user);
+    getRequest(userURL, mainElement, function (element, user) {
+      element.innerHTML = templates.user(user);
       getUserPosts(userid);
     });
   };
 
-  var getUserPosts = function (userid) {
-    var user_posts_url = base_url + "users/" + userid + "/posts";
-    var element = document.getElementById("user-posts")
+  var postSignUp = function (event) {
+    event.preventDefault();
 
-    getRequest(user_posts_url, element, function (element, posts) {
-      element.innerHTML = processPosts(posts);
-    });
+    var email = document.getElementById("email-field").value;
+    var username = document.getElementById("username-field").value;
+    var password = document.getElementById("password-field").value;
+    var xhr = new XMLHttpRequest();
+    var url = baseURL + "users";
+    var body = { user: { email: email, username: username, password: password } };
+    xhr.open("post", url)
+
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // why does a 500 go here?
+        logIn(JSON.parse(xhr.responseText));
+        getPosts();
+      } else if (xhr.responseText) {
+        renderErrors(xhr.responseText);
+      }
+    };
+
+    xhr.send(JSON.stringify(body));
+  };
+
+  var renderErrors = function (errorText) {
+    errors = JSON.parse(errorText).errors;
+
+    for (error in errors) { console.log(error) }
+  };
+
+  // Authentication
+
+  var logIn = function (user) {
+    localStorage.setItem('auth-email', user.email);
+    localStorage.setItem('auth-token', user.auth_token);
+    localStorage.setItem('username', user.username);
+    localStorage.setItem('user-id', user.id);
+
+    showLoggedInState();
+  };
+
+  var isLoggedIn = function () {
+    if (localStorage["auth-email"] &&
+        localStorage["auth-token"] &&
+        localStorage["username"] &&
+        localStorage["user-id"]) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var showLogIn = function () {
+    mainElement.innerHTML = templates.logInForm();
+
+    var logInButton = document.getElementById("log-in-button");
+
+    logInButton.onclick = postLogIn;
+  };
+
+  var showSignUp = function () {
+    mainElement.innerHTML = templates.signUpForm();
+
+    var signUpButton = document.getElementById("sign-up-button");
+
+    signUpButton.onclick = postSignUp;
+  };
+
+  var showLoggedInState = function () {
+    logInLink.innerHTML = localStorage["username"];
+    logInLink.onclick = function (event){
+      getUser(localStorage["user-id"]);
+    };
+
+    signUpLink.innerHTML = "Log Out";
+    signUpLink.onclick = postLogOut;
+  };
+
+  var showLoggedOutState = function () {
+    logInLink.innerHTML = "Log In";
+    logInLink.onclick = showLogIn;
+
+    signUpLink.innerHTML = "Sign Up";
+    signUpLink.onclick = showSignUp;
+  };
+
+  var postLogIn = function (event) {
+    event.preventDefault();
+
+    var username = document.getElementById("username-field").value;
+    var password = document.getElementById("password-field").value;
+    var xhr = new XMLHttpRequest();
+    var url = baseURL + "session";
+    var body = { user: { username: username, password: password } };
+    xhr.open("post", url)
+
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      var errors = document.getElementById("log-in-errors");
+
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        logIn(JSON.parse(xhr.responseText));
+        getPosts();
+      } else if (xhr.responseText) {
+        renderErrors(xhr.responseText);
+      }
+    };
+
+    xhr.send(JSON.stringify(body));
+  };
+
+  var postLogOut = function () {
+    var xhr = new XMLHttpRequest();
+    var url = baseURL + "session";
+    xhr.open("delete", url)
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 204) {
+        localStorage.removeItem("auth-email");
+        localStorage.removeItem("auth-token");
+        localStorage.removeItem("username");
+        localStorage.removeItem("user-id");
+
+        showLoggedOutState();
+        getPosts();
+      } else if (xhr.responseText) {
+        renderErrors(xhr.responseText);
+      }
+    };
+
+    xhr.send(null);
   };
 
   window.addEventListener("load", getPosts);
-  brandLink.addEventListener("click", getPosts);
-  homeLink.addEventListener("click", getPosts);
+  brandLink.onclick = getPosts;
+  homeLink.onclick = getPosts;
+  usersLink.onclick = getUsers;
+  logInLink.onclick = showLogIn;
+  signUpLink.onclick = showSignUp;
 })(window, window.document);
--- vanilla-js/templates.js
+++ vanilla-js/templates.js
@@ -12,7 +12,7 @@
       updated_at: postResponse.updated_at || ""
     };
 
-    var output =
+    var postHTML =
       '<div class="row">'
     +   '<div class="col-md-12 card card-block">'
     +     '<h4 class="card-title">'
@@ -31,7 +31,7 @@
     +   '</div>'
     + '</div>';
 
-    return output;
+    return postHTML;
   };
 
   window.templates.user = function (userResponse) {
@@ -42,7 +42,7 @@
       admin: userResponse.admin || ""
     };
 
-    var output =
+    var userHTML =
       '<div class="row">'
     +   '<div class="col-sm-4">'
     +     '<p class="lead">'
@@ -58,10 +58,100 @@
     +   '</div>'
     + '</div>';
 
-    return output;
+    return userHTML;
+  };
+
+  window.templates.users = function (users) {
+    return users.map(function (user) {
+      return '<div class="row">'
+      +   '<div class="col-sm-6 offset-sm-3">'
+      +     '<p class="lead">'
+      +       '<a href="#" class="user-link" data-userid="' + user.id + '">'
+      +          user.username
+      +       '</a>'
+      +     '</p>'
+      +   '</div>'
+      + '</div>';
+    }).join("\n");
+  };
+
+  window.templates.signUpForm = function () {
+    var signUpFormHTML =
+      '<div class="row">'
+    +   '<div class="col-sm-6 offset-sm-3">'
+    +     '<h3>Log In</h3>'
+    +     '<form>'
+    +       '<div class="form-group">'
+    +         '<label for="username-field">Username</label>'
+    +         '<input type="text" class="form-control" id="username-field">'
+    +       '</div>'
+    +       '<div class="form-group">'
+    +         '<label for="email-field">Email</label>'
+    +         '<input type="text" class="form-control" id="email-field">'
+    +       '</div>'
+    +       '<div class="form-group">'
+    +         '<label for="password-field">Password</label>'
+    +         '<input type="password" class="form-control" id="password-field">'
+    +       '</div>'
+    +       '<div class="form-group">'
+    +         '<label for="confirm-password-field">Confirm Password</label>'
+    +         '<input type="password" class="form-control" id="confirm-password-field">'
+    +       '</div>'
+    +       '<button type="submit" class="btn btn-primary" id="sign-up-button">Sign Up</button>'
+    +     '</form>'
+    +   '</div>'
+    + '</div>';
+
+    return signUpFormHTML;
+  };
+
+  window.templates.logInForm = function () {
+    var logInFormHTML =
+      '<div class="row">'
+    +   '<div class="col-sm-6 offset-sm-3">'
+    +     '<h3>Log In</h3>'
+    +     '<hr>'
+    +     '<div id="log-in-errors"></div>'
+    +     '<form>'
+    +       '<div class="form-group">'
+    +         '<label for="username-field">Username</label>'
+    +         '<input type="text" class="form-control" id="username-field">'
+    +       '</div>'
+    +       '<div class="form-group">'
+    +         '<label for="password-field">Password</label>'
+    +         '<input type="password" class="form-control" id="password-field">'
+    +       '</div>'
+    +       '<button type="submit" class="btn btn-primary" id="log-in-button">Log In</button>'
+    +     '</form>'
+    +   '</div>'
+    + '</div>';
+
+    return logInFormHTML;
+  };
+
+  window.templates.postForm = function () {
+    var postFormHTML =
+      '<div class="row">'
+    +   '<div class="col-md-12 card card-block">'
+    +   '<h4 class="card-title">New Post</h4>'
+    +   '<form>'
+    +     '<div class="form-group">'
+    +       '<input type="text" class="form-control" id="title-field" placeholder="Title">'
+    +     '</div>'
+    +     '<div class="form-group">'
+    +       '<textarea class="form-control" id="content-field" rows="3" placeholder="Content"></textarea>'
+    +     '</div>'
+    +     '<button type="submit" class="btn btn-primary" id="submit-post-button">Submit</button>'
+    +   '</form>'
+    +   '</div>'
+    +   '</div>'
+    + '</div>';
+
+    return postFormHTML;
   };
 
   window.templates.spinner = function () {
+    // from http://tobiasahlin.com/spinkit/
     var spinnerHTML =
       '<div class="sk-circle">'
     +   '<div class="sk-circle1 sk-child"></div>'
