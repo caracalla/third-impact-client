532bb118b564bff98cf2252821a0511dcb1a0a98
James Anderson <caracallarex@gmail.com>
2016-12-17 08:14:06 +0000

    Remove jquery, add main index

--- jquery/js/main.js
+++
@@ -1,5 +0,0 @@
-(function($, window, document, undefined) {
-  'use strict';
-
-  $(document).ready(ThirdImpact.init);
-}(window.jQuery, window, window.document));
--- v2j/js/main.js
+++
@@ -1,8 +0,0 @@
-(function (window, document, $, undefined) {
-  'use strict';
-
-  $(document).ready(window.models.post.index);
-
-  window.baseURL = "http://api.caracal.la/";
-  window.mainElement = document.getElementById("main");
-})(window, window.document, window.jQuery);
